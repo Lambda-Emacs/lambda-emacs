c95857819e6fc5711dad22c06d70ac0e2f78dd4d
Colin McLear <mclear@fastmail.com>
2022-03-18 18:21:21 -0400

    First commit

---
+++ early-init.el
@@ -0,0 +1,179 @@
+;;; early-init.el --- summary -*- lexical-binding: t; no-byte-compile: t; mode: emacs-lisp; coding:utf-8; fill-column: 80 -*-
+;; Author: Colin McLear
+;; Maintainer: Colin McLear
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This is the early-init file. Only for use with emacs 27 or higher. See
+;; https://www.reddit.com/r/emacs/comments/7yns85/emacs_adds_support_for_a_second_read_earlier_init/
+;; and https://lists.gnu.org/archive/html/emacs-devel/2017-10/msg00372.html for
+;; more information.
+
+;;; Code:
+
+;;;; Speed up startup
+;; Help speed up emacs initialization See
+;; https://blog.d46.us/advanced-emacs-startup/ and
+;; http://tvraman.github.io/emacspeak/blog/emacs-start-speed-up.html and
+;; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+;; This will be set back to normal at the end of the init file
+
+(defvar lem-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+
+;;;; Garbage collection
+
+;; Defer garbage collection further back in the startup process We'll lower this
+;; to a more reasonable number at the end of the init process (at end of
+;; init.el)
+
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Adjust garbage collection thresholds during startup, and thereafter
+;; See http://akrl.sdf.org https://gitlab.com/koral/gcmh
+
+(defmacro k-time (&rest body)
+  "Measure and return the time it takes evaluating BODY."
+  `(let ((time (current-time)))
+     ,@body
+     (float-time (time-since time))))
+
+;; When idle for 15sec run the GC no matter what.
+(defvar k-gc-timer
+  (run-with-idle-timer 15 t
+                       (lambda ()
+                         (let ((inhibit-message t))
+                           (message "Garbage Collector has run for %.06fsec"
+                                    (k-time (garbage-collect)))))))
+
+;;;; Native Comp
+
+;; See https://github.com/jimeh/build-emacs-for-macos#native-comp
+;; https://akrl.sdf.org/gccemacs.html#org335c0de
+;; https://github.com/emacscollective/no-littering/wiki/Setting-gccemacs'-eln-cache
+;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=53891
+;; https://emacs.stackexchange.com/a/70478/11934
+
+;; See if native-comp is available
+(when (not (and (fboundp 'native-comp-available-p)
+                (native-comp-available-p)))
+  (message "Native complation is *not* available"))
+
+;; Set eln-cache dir
+;; NOTE this only works for emacs≥29
+;; TODO add conditional if not emacs 29...
+(when (fboundp 'startup-redirect-eln-cache)
+  (if (version< emacs-version "29")
+      (add-to-list 'native-comp-eln-load-path (convert-standard-filename (expand-file-name ".local/temp/cache/eln-cache/" user-emacs-directory)))
+    (startup-redirect-eln-cache (convert-standard-filename (expand-file-name ".local/temp/cache/eln-cache/" user-emacs-directory)))))
+
+;; Silence nativecomp warnings popping up
+(setq native-comp-async-report-warnings-errors nil)
+
+;; Native-comp settings
+(setq native-comp-speed 2
+      native-comp-deferred-compilation t)
+
+;;;; Set C Directory
+;; NOTE this assumes that the C source files are included with emacs.
+;; This depends on the build process used.
+;; For one example see https://github.com/mclear-tools/build-emacs-macos
+(setq find-function-C-source-directory "/Applications/Emacs.app/Contents/Resources/src")
+
+;;;; Prefer Newer files
+;; Prefer newer versions of files
+(setq load-prefer-newer t)
+
+;;;; Byte Compile Warnings
+;; Disable certain byte compiler warnings to cut down on the noise. This is a
+;; personal choice and can be removed if you would like to see any and all byte
+;; compiler warnings.
+(setq byte-compile-warnings '(not free-vars unresolved noruntime lexical make-local obsolete))
+;;;; Check Errors
+;; Don't produce backtraces when errors occur.
+;; This can be set to `t' interactively when debugging.
+(setq debug-on-error nil)
+
+;;;; When-let errors
+;; https://github.com/alphapapa/frame-purpose.el/issues/3
+(eval-and-compile
+  (when (version< emacs-version "26")
+    (with-no-warnings
+      (defalias 'when-let* #'when-let)
+      (function-put #'when-let* 'lisp-indent-function 1)
+      (defalias 'if-let* #'if-let)
+      (function-put #'if-let* 'lisp-indent-function 2))))
+
+;;;; Variable Binding Depth
+;; This variable controls the number of lisp bindings that can exists at a time.
+;; We should make it fairly large for modern machines.
+;; https://www.reddit.com/r/emacs/comments/9jp9zt/anyone_know_what_variable_binding_depth_exceeds/
+(setq max-specpdl-size 13000)
+
+;;;; Package settings
+;; We use straight not package.el for all package loading.
+;; So we don't need package.el loaded at startup (or at all).
+(setq package-enable-at-startup nil)
+(advice-add #'package--ensure-init-file :override #'ignore)
+
+;; Do not allow loading from the package cache (same reason).
+(setq package-quickstart nil)
+
+;;;; Clean View
+;; UI - Disable visual cruft
+
+;; Resizing the Emacs frame can be an expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq-default frame-title-format nil)
+(setq-default frame-inhibit-implied-resize t)
+;; Disable start-up screen
+(setq-default inhibit-startup-screen t)
+(setq-default inhibit-startup-message t)
+;; We'll provide our own splash screen, thanks
+(setq-default inhibit-splash-screen t)
+;; No message in initial scratch buffer
+(setq-default initial-scratch-message nil)
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+;; Disable tool, menu, and scrollbars. these are just clutter (the scrollbar
+;; also impacts performance). I am intentionally not calling `menu-bar-mode',
+;; `tool-bar-mode', and `scroll-bar-mode' (see doom for inspiration) because
+;; they do extra and unnecessary work that can be more concisely and efficiently
+;; expressed with these six lines:
+(push '(menu-bar-lines . 0)   default-frame-alist)
+(push '(tool-bar-lines . 0)   default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+;; And set these to nil so users don't have to toggle the modes twice to
+;; reactivate them.
+(setq menu-bar-mode nil
+      tool-bar-mode nil
+      scroll-bar-mode nil)
+
+;; Fundamental mode at startup.
+;; This helps with load-time since no extra libraries are loaded.
+(setq initial-major-mode 'fundamental-mode)
+
+;; Echo buffer -- don't display any message
+;; https://emacs.stackexchange.com/a/437/11934
+(defun display-startup-echo-area-message ()
+  (message ""))
+
+;;; early-init.el ends here
---
+++ init.el
@@ -0,0 +1,312 @@
+;;; init.el  -*- lexical-binding: t; mode: emacs-lisp; coding:utf-8; fill-column: 80 -*-
+;;; Commentary:
+;; This is the base init file to load the entire emacs config. For ease of
+;; navigation use outline-mode to cycle headlines open and closed (<Tab> and
+;; <S-Tab>) to navigate through sections, and "imenu" to locate individual
+;; use-package definitions.
+
+;;; Code:
+
+;;;; Startup
+
+;;;;; System Variables
+;; Check the system used
+(defconst sys-linux   (eq system-type 'gnu/linux))
+(defconst sys-mac     (eq system-type 'darwin))
+(defconst sys-bsd     (or sys-mac (eq system-type 'berkeley-unix)))
+(defconst sys-win     (memq system-type '(cygwin windows-nt ms-dos)))
+
+;;;;; Directory Variables
+;;  We're going to define a number of directories that are used throughout this
+;;  configuration to store different types of files. This is a bit like the
+;;  `no-littering' package, and allows us to keep `user-emacs-directory' tidy.
+
+(defconst lem-emacs-dir (expand-file-name user-emacs-directory)
+  "The path to the emacs.d directory.")
+
+(defconst lem-local-dir (concat lem-emacs-dir ".local/")
+  "The root directory for local Emacs files. This contains all
+elisp libraries as well as non-essential and/or ephemeral files.
+There are two main directories -- lem-library-dir, which
+contains all libraries and packages, and lem-temp-dir, which
+contains non-essential files and emphemera.")
+
+(defconst lem-library-dir (concat lem-local-dir "lambda-library/")
+  "The directory for Lisp libraries. This will house any personal
+Lisp libraries as well as all setup libraries and external packages.")
+
+(defconst lem-private-dir (concat lem-library-dir "lambda-private/")
+  "Storage for personal elisp, scripts, and any other private files.")
+
+(defconst lem-setup-dir (concat lem-library-dir "lambda-setup/")
+  "The storage location of the setup-init files.")
+
+(defconst lem-temp-dir (concat lem-local-dir "temp/")
+  "The directory for non-essential file storage. Used by
+  `lem-etc-dir' and `lem-cache-dir'.")
+
+(defconst lem-etc-dir (concat lem-temp-dir "etc/")
+  "The directory for non-volatile storage. These are not deleted or
+tampered with by emacs functions. Use this for dependencies like
+servers or config files that are stable (i.e. it should be
+unlikely that you need to delete them if something goes wrong).")
+
+(defconst lem-cache-dir (concat lem-temp-dir "cache/")
+  "The directory for volatile storage. Use this for transient files
+that are generated on the fly like caches and ephemeral/temporary
+files. Anything that may need to be cleared if there are
+problems.")
+
+;;;;; User Configuration Variables
+;; Find the user configuration file
+(defvar lem-config-file (expand-file-name "lambda-config.el" lem-private-dir)
+  "The user's configuration file.")
+
+;;;;; Path Settings
+;; Directory paths
+(dolist (dir (list lem-local-dir lem-library-dir lem-etc-dir lem-cache-dir lem-private-dir lem-setup-dir))
+  (unless (file-directory-p dir)
+    (make-directory dir t)))
+
+;;;;; Load Path
+;; Add config files to load-path
+(eval-and-compile
+  (progn
+    (push lem-setup-dir load-path)))
+
+;; Set PATH properly for emacs. This should make a package like
+;; `exec-path-from-shell' unnecessary
+
+;; Set local bin path
+(defconst lem-local-bin (concat (getenv "HOME") "/bin") "Local execs.")
+;; If on a mac using homebrew set path correctly
+;; NOTE the location of homebrew depends on whether we're on mac silicon
+(when (shell-command "command -v brew")
+  (defconst homebrew (if (string= (shell-command-to-string "arch") "arm64") "/opt/homebrew/bin/"
+                       "/usr/local/bin/") "Path to homebrew packages."))
+(defconst usr-local-bin "/usr/local/bin" "System bin.")
+(defconst usr-local-sbin "/usr/local/sbin" "System sbin.")
+(setenv "PATH" (concat homebrew ":" usr-local-bin ":" usr-local-sbin ":" (getenv "PATH") ":" lem-local-bin))
+(setq exec-path (append exec-path (list homebrew lem-local-bin usr-local-sbin usr-local-bin)))
+
+;;;; Package Settings
+;; Use straight and use-package to manage settings. Defer package loading as
+;; much as possible to either the after-init-hook or after some number of
+;; seconds of idle. This helps especially when doing, e.g., a quick
+;; restart-and-check of something in emacs.
+
+;;;;; Straight
+;;;;;; Straight settings
+;; Use straight.el to install all packages
+;; https://github.com/raxod502/straight.el
+;; Don't check packages on startup
+(setq straight-check-for-modifications '(check-on-save find-when-checking))
+;; Set branch
+(setq straight-repository-branch "develop")
+;; Set dir
+(setq straight-base-dir lem-library-dir)
+;; Use use-package
+(setq straight-use-package-by-default t)
+;; Check updates manually
+(setq straight-vc-git-auto-fast-forward nil)
+;; Avoid problems with straight building with native-comp
+;; See https://github.com/raxod502/straight.el/issues/757
+(setq native-comp-deferred-compilation-deny-list nil)
+;; Tell straight.el about the profiles we are going to be using.
+(setq straight-profiles
+      '((nil . "default.el")
+        ;; Packages which are pinned to a specific commit.
+        (pinned . "pinned.el")))
+
+;;;;;; Bootstrap straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" straight-base-dir))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;;;;;; Straight-X
+;; Use experimental straight commands
+(require 'straight-x)
+;; https://github.com/raxod502/straight.el#how-do-i-pin-package-versions-or-use-only-tagged-releases
+(autoload #'straight-x-freeze-versions "straight-x")
+;; package updates
+;; use this workflow?
+;; https://github.com/raxod502/straight.el/issues/354#issuecomment-465305063
+(autoload #'straight-x-pull-all "straight-x")
+;; async fetch
+(autoload #'straight-x-fetch-all "straight-x")
+
+;;;;;; Straight Helper Functions
+;; delete .DS_Store before prune
+(advice-add 'straight-prune-build :before #'(lambda () (move-file-to-trash "/Users/roambot/.emacs.d/.local/straight/build/.DS_Store")))
+
+;;;;; Use-Package
+;; install use package
+(straight-use-package 'use-package)
+;; settings
+(setq use-package-always-defer nil
+      use-package-verbose t
+      use-package-minimum-reported-time 0
+      use-package-enable-imenu-support t
+      use-package-expand-minimally nil
+      use-package-always-ensure nil)
+
+;;;;; El-Patch
+;; Package for helping advise/modify features of other packages
+
+(use-package el-patch
+  :straight t
+  :config
+  (setq el-patch-enable-use-package-integration t))
+
+;;;; Security
+;; Properly verify outgoing ssl connections.
+;; See https://glyph.twistedmatrix.com/2015/11/editor-malware.html
+
+(use-package gnutls
+  :straight nil
+  :defer 1
+  :init
+  (setq gnutls-verify-error t
+        gnutls-min-prime-bits 3072))
+
+;;;; Benchmark Init
+(use-package benchmark-init
+  ;; demand when using
+  ;; :demand t
+  :defer t
+  :config
+  ;; To disable collection of benchmark data after init is done.
+  (add-hook 'emacs-startup-hook 'benchmark-init/deactivate))
+
+;;;; Auto-compile
+;; Automatically byte-recompile changed elisp libraries
+(use-package auto-compile
+  :defer 1
+  :custom
+  (auto-compile-display-buffer nil)
+  (auto-compile-mode-line-counter nil)
+  (auto-compile-use-mode-line nil)
+  (auto-compile-update-autoloads t)
+  :config
+  (auto-compile-on-load-mode)
+  (auto-compile-on-save-mode))
+
+;;;; Command Line Switches
+
+;; Conditionally load parts of config depending on command line switches.
+;; This allows startup with a clean emacs that still recognizes straight.
+;; Helpful for testing packages.
+;; Use (straight-use-package) command to selectively load packages.
+;; See https://emacs.stackexchange.com/a/34909/11934
+;; For function switch see https://stackoverflow.com/a/4065412/6277148
+
+(defvar lem-config-switches
+  '("clean"
+    "core"
+    "test")
+  "Custom switches for conditional loading from command line.
+`clean' loads only the init.el file w/no personal config; `core'
+loads the set of modules set in `lem-core-modules'; `test' loads
+only a setup-test.el' file for easy testing.")
+
+(defvar lem-core-modules
+  '(lem-setup-settings
+    lem-setup-libraries
+    lem-setup-functions
+    lem-setup-macros)
+  "Core modules for lambda-emacs.")
+
+(defun lem--emacs-switches (switch)
+  "Depending on command line argument SWITCH, load Emacs with minimal settings & no modules; useful for testing."
+  (let ((found-switch (member switch command-line-args)))
+    (setq command-line-args (delete switch command-line-args))
+    found-switch))
+
+;;;; Emacs Build Version
+;; When built emacs has git-version patch
+;; to include git sha1 in emacs-version string
+(setq site-lisp "/Applications/Emacs.app/Contents/Resources/site-lisp/")
+(when (file-exists-p (concat site-lisp "emacs-git-version.el"))
+  (require 'emacs-git-version))
+
+(defun lem/emacs-version ()
+  "Print emacs-version and put emacs-version string on the kill ring"
+  (interactive)
+  (let ((emacs (emacs-version)))
+    (message (emacs-version))
+    (kill-new emacs)))
+
+;;;; Outline Navigation
+;; Packages to help with navigating
+;; I used to use outshine.el but it was overkill -- these packages are much smaller/simpler
+
+(use-package outline
+  :straight (:type built-in)
+  :hook (prog-mode . outline-minor-mode)
+  :bind (:map outline-minor-mode-map
+         ("<tab>"   . outline-cycle)
+         ("S-<tab>" . outline-cycle-buffer)
+         ("M-j"     . outline-move-subtree-down)
+         ("M-k"     . outline-move-subtree-up)
+         ("M-h"     . outline-promote)
+         ("M-l"     . outline-demote))
+  :config
+  (add-hook 'emacs-lisp-mode-hook
+            (lambda ()
+              ;; prevent `outline-level' from being overwritten by `lispy'
+              (setq-local outline-level #'outline-level)
+              ;; setup heading regexp specific to `emacs-lisp-mode'
+              (setq-local outline-regexp ";;;\\(;* \\)")
+              ;; heading alist allows for subtree-like folding
+              (setq-local outline-heading-alist
+                          '((";;; " . 1)
+                            (";;;; " . 2)
+                            (";;;;; " . 3)
+                            (";;;;;; " . 4)
+                            (";;;;;;; " . 5))))))
+
+;; Make outline faces look better
+(use-package outline-minor-faces
+  :after outline
+  :config (add-hook 'outline-minor-mode-hook
+                    'outline-minor-faces-add-font-lock-keywords))
+
+;;;; Load Configuration
+(unless (lem--emacs-switches "-clean")
+  (message "Loading config.el")
+  ;; Load the user configuration file if it exists
+  (when (file-exists-p lem-config-file)
+    (load lem-config-file nil 'nomessage)))
+
+;; Switch for testing packages
+(when (lem--emacs-switches "-test")
+  (require 'setup-test))
+
+;; Switch for loading only core modules
+(if (lem--emacs-switches "-core")
+    (progn
+      (message "Loading core modules *only*")
+      (mapcar 'require lem-core-modules)))
+
+;;;; After Startup
+
+
+;; reset file-name-handler-alist
+(add-hook 'emacs-startup-hook (lambda ()
+                                (setq file-name-handler-alist lem-file-name-handler-alist)
+                                ;; reset garbage collection
+                                (setq gc-cons-threshold 800000)
+                                ;; Startup time
+                                (message (format ";; ======================================================\n;; Emacs ready in %.2f seconds with %d garbage collections.\n;; ======================================================"
+                                                 (float-time
+                                                  (time-subtract after-init-time before-init-time)) gcs-done)
+                                         (put 'narrow-to-page 'disabled nil))))
