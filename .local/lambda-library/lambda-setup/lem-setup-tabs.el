1f1eaa1e935c82dc9f87d5e0db049545ac449e23
Colin McLear <mclear@fastmail.com>
2022-04-16 01:30:37 -0400

    Consolidate tabs/workspaces files

--- .local/lambda-library/lambda-setup/lem-setup-tabs.el
+++ .local/lambda-library/lambda-setup/lem-setup-tabs.el
@@ -20,7 +20,11 @@
 ;;; Commentary:
 
 ;; Tabs -- in some ways tabs are not very Emacsy, but tab-bar is great for
-;; window & buffer management. The setup for tabs here is focused on tabs for window configurations related to a "workspace" -- i.e. a project or other set of windows. See `lem-setup-workspaces'.
+;; window & buffer management. The setup for tabs here is focused on tabs for
+;; window configurations related to a "workspace" -- i.e. a project or other set
+;; of windows. A workspace or "tabspace" (see the package below) is just a tab
+;; with an isolated set of buffers (see also the consult function). 𝛌-Emacs (and
+;; 𝛌-Themes) sets things up so that these tabs/workspaces are nicely displayed.
 
 ;;; Code:
 
@@ -34,20 +38,27 @@
              tab-bar-switch-to-next-tab
              tab-bar-switch-to-prev-tab)
   :custom
+  (tab-bar-show 1)
   ;; Unless another file/buffer is designated, start from scratch buffer
   (tab-bar-new-tab-choice "*scratch*")
   (tab-bar-select-tab-modifiers '(super))
   (tab-bar-close-tab-select 'recent)
   (tab-bar-new-tab-to 'rightmost)
   (tab-bar-close-last-tab-choice 'tab-bar-mode-disable)
-  (tab-bar-show nil)
+  (tab-bar-separator " ")
+  (tab-bar-new-button nil)
+  (tab-bar-close-button nil)
+  (tab-bar-format '(tab-bar-format-history
+                    tab-bar-format-tabs
+                    tab-bar-separator
+                    tab-bar-format-add-tab))
   :config
   ;; https://protesilaos.com/codelog/2020-08-03-emacs-custom-functions-galore/
   (defun lem/tab-bar-select-tab-dwim ()
     "Do-What-I-Mean function for getting to a `tab-bar-mode' tab.
 If no other tab exists, create one and switch to it.  If there is
 one other tab (so two in total) switch to it without further
-questions.  Else use completion to select the tab to switch to."
+questions.  Otherwise use completion to select the tab."
     (interactive)
     (let ((tabs (mapcar (lambda (tab)
                           (alist-get 'name tab))
@@ -61,60 +72,48 @@ questions.  Else use completion to select the tab to switch to."
               (completing-read "Select tab: " tabs nil t))))))
   (setq tab-bar-mode t))
 
-;;;;; Tab Bar Echo
-;; Use echo area for tab name display
-(use-package tab-bar-echo-area
-  :straight (:type git :host github :repo "fritzgrabo/tab-bar-echo-area")
-  :bind (:map tab-prefix-map
-         ("c" . tab-bar-echo-area-display-tab-name)
-         ("a" . tab-bar-echo-area-display-tab-names))
-  :config
-  (setq tab-bar-echo-area-trigger-display-functions nil)
-  (tab-bar-echo-area-mode 1))
-
-;;;;; Echo-Bar
-;; Display info in the echo area -- using just for tabs/workspaces right now
-(use-package echo-bar
-  :straight (echo-bar :type git :host github :repo "qaiviq/echo-bar.el")
-  :config
-  (setq echo-bar-function #'lem--echo-bar
-        echo-bar-minibuffer t)
-  (echo-bar-enable))
-
-(defun lem--echo-bar ()
-  "Function for echo bar"
-  ;; Show tabs
-  (lem-echo-bar/tabs))
-
-(defun lem-echo-bar/tabs ()
-  "Display tab names in the echo area. Depends on `tab-bar-echo-area'"
-  (interactive)
-  (require 'tab-bar-echo-area)
-  (let* ((tab-bar-format (or tab-bar-echo-area-format (and (boundp 'tab-bar-format) tab-bar-format)))
-         (keymap (funcall tab-bar-echo-area-make-keymap-function))
-         (keymap-elements (seq-filter #'tab-bar-echo-area--keymap-element-type (cdr keymap))))
-    (if-let ((tab-names (tab-bar-echo-area--processed-tab-names keymap-elements))
-             (format-string (cond ((functionp tab-bar-echo-area-display-tab-names-format-string)
-                                   (funcall tab-bar-echo-area-display-tab-names-format-string keymap-elements))
-                                  ((stringp tab-bar-echo-area-display-tab-names-format-string)
-                                   tab-bar-echo-area-display-tab-names-format-string)
-                                  (t "%s"))))
-        (format "%s" (string-join tab-names)))))
-
-(defun echo-bar--list-workspaces ()
-  "Return a list of `tab-bar' tabs/workspaces."
-  (require 'tab-bar)
-  (let ((tab-names (mapcar (lambda (tab) (alist-get 'name tab)) (tab-bar-tabs))))
-    (mapconcat 'identity tab-names " ")))
-
-;;;;; Tab Bookmark
+;;;; Tab Bookmark
 ;; Bookmark window configurations in a tab
-;; NOTE: would be good to get this working with emacs-workspaces
+;; NOTE: would be good to get this working with tabspaces
 (use-package tab-bookmark
   :straight (:type git :host github :repo "minad/tab-bookmark")
   :commands (tab-bookmark tab-bookmark-open)
   :bind (:map project-prefix-map
          ("m" . tab-bookmark)))
 
+;;;; Workspaces
+
+(use-package tabspaces
+  ;; :straight (:type git :host github :repo "mclear-tools/emacs-workspaces")
+  :straight (:local-repo "/Users/roambot/bin/lisp-projects/tabspaces")
+  :hook (after-init . tabspaces-mode)
+  ;; Add some functions to the project map
+  :bind (:map project-prefix-map
+         ("p" . tabspaces-open-existing-project-and-workspace)
+         ("n" . tabspaces-create-new-project-and-workspace))
+  :custom
+  (tabspaces-use-filtered-buffers-as-default t))
+
+;;;; Per Workspace Buffers with Consult
+;; Filter Buffers for Consult-Buffer
+
+(with-eval-after-load 'consult
+  ;; hide full buffer list (still available with "b")
+  (consult-customize consult--source-buffer :hidden t :default nil)
+  ;; set consult-workspace buffer list
+  (defvar consult--source-workspace
+    (list :name     "Workspace Buffers"
+          :narrow   ?w
+          :category 'buffer
+          :state    #'consult--buffer-state
+          :default  t
+          :items    (lambda ()
+                      (tabspaces--tab-bar-buffer-name-filter ((lambda () (consult--buffer-query :sort 'visibility
+                                                                                           :as #'buffer-name))))))
+
+    "Set workspace buffer list for consult-buffer.")
+  (push consult--source-workspace consult-buffer-sources))
+
+;;; Provide
 (provide 'lem-setup-tabs)
 ;;; lem-setup-tabs.el ends here
