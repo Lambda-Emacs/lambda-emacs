1e2339918ef5d930b5c7debcf625b374dd88ac67
Colin McLear <mclear@fastmail.com>
2022-05-08 01:55:11 -0400

    Revise scratch settings

--- .local/lambda-library/lambda-setup/lem-setup-scratch.el
+++ .local/lambda-library/lambda-setup/lem-setup-scratch.el
@@ -1,22 +1,23 @@
 ;; Better scratch settings
 ;; See https://www.reddit.com/r/emacs/comments/4cmfwp/scratch_buffer_hacks_to_increase_its_utility/
 
-(defun bury-scratch ()
+(defun lem--bury-scratch ()
+  "Don't kill scratch buffer, bury it."
   (if (eq (current-buffer) (get-buffer "*scratch*"))
       (progn (bury-buffer)
              nil)
     t))
 
-(add-hook 'kill-buffer-query-functions 'bury-scratch)
+(add-hook 'kill-buffer-query-functions 'lem--bury-scratch)
 
-(defun save-persistent-scratch ()
-  "Save the contents of *scratch*"
+(defun lem--save-persistent-scratch ()
+  "Save the contents of *scratch*."
   (with-current-buffer (get-buffer-create "*scratch*")
     (write-region (point-min) (point-max)
                   (concat lem-cache-dir "scratch"))))
 
-(defun load-persistent-scratch ()
-  "Reload the scratch buffer"
+(defun lem--load-persistent-scratch ()
+  "Reload the scratch buffer."
   (let ((scratch-file (concat lem-cache-dir "scratch")))
     (if (file-exists-p scratch-file)
         (with-current-buffer (get-buffer "*scratch*")
@@ -29,10 +30,10 @@
 
 
 ;; Hooks for loading and saving the scratch buffer
-(add-hook 'after-init-hook 'load-persistent-scratch)
-(add-hook 'kill-emacs-hook 'save-persistent-scratch)
+(add-hook 'after-init-hook 'lem--load-persistent-scratch)
+(add-hook 'kill-emacs-hook 'lem--save-persistent-scratch)
 ;; Save scratch buffer every 5 minutes (300 seconds)
-(run-with-idle-timer 300 t 'save-persistent-scratch)
+(run-with-idle-timer 300 t 'lem--save-persistent-scratch)
 
 ;;; End Setup-Scratch
 (provide 'lem-setup-scratch)
