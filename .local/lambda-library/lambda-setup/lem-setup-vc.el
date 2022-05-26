eb4707253a1d1bab21d070d909955c7396a4d5cc
Colin McLear <mclear@fastmail.com>
2022-05-26 15:44:19 -0400

    Fix git setting for macos

--- .local/lambda-library/lambda-setup/lem-setup-vc.el
+++ .local/lambda-library/lambda-setup/lem-setup-vc.el
@@ -43,7 +43,10 @@
   :config
   (setq magit-log-margin '(t "%Y-%m-%d.%H:%M:%S "  magit-log-margin-width nil 18))
   (setq magit-refresh-status-buffer t)
-  (setq magit-git-executable "/opt/homebrew/bin/git")
+  ;; Set git for macos & homebrew
+  (if (and sys-mac homebrew)
+      (setq magit-git-executable (concat homebrew "git"))
+    (setq magit-git-executable "/usr/bin/git"))
   ;; Fine grained diffs
   (setq magit-diff-refine-hunk t)
   ;; control magit initial visibility
