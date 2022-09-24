80cd32ad1c8fca0fc541d43915ba1256e271c4d6
Colin McLear <mclear@fastmail.com>
2022-09-24 10:44:22 -0400

    Add header info for init file

--- init.el
+++ init.el
@@ -1,4 +1,23 @@
 ;;; init.el  -*- lexical-binding: t; mode: emacs-lisp; coding:utf-8; fill-column: 80 -*-
+;; Author: Colin McLear
+;; Maintainer: Colin McLear
+;; Version: 0.3
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; This is the base init file to load the entire emacs config. For ease of
 ;; navigation use outline-mode to cycle headlines open and closed (<Tab> and
