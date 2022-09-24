649611c9118254dd25824eff6af7134c4a39f235
Colin McLear <mclear@fastmail.com>
2022-09-23 21:13:10 -0400

    Remove help hacks
    - helpful has been revised so we don't need these any more
    - fix commentary keybinding

--- .local/lambda-library/lambda-setup/lem-setup-help.el
+++ .local/lambda-library/lambda-setup/lem-setup-help.el
@@ -81,10 +81,6 @@
 ;;;;; Better Help with Helpful
 ;; Better help info
 ;; Much better lookup both in details and headings/aesthetics
-
-;; NOTE: emacs 29 has a breaking change so using el-patch to keep helpful working
-;; see https://github.com/Wilfred/helpful/pull/283
-
 (use-package helpful
   :defer t
   :bind (;; Remap standard commands.
@@ -94,29 +90,9 @@
          ([remap describe-symbol]    . helpful-symbol)
          ([remap describe-key]       . helpful-key)
          ([remap describe-command]   . helpful-command)
-         ("C-h C-l" . #'find-library))
-  :init
-  ;; HACK: - see https://github.com/hlissner/doom-emacs/issues/6063
-  (defvar read-symbol-positions-list nil)
-  :config/el-patch
-  (defun helpful--autoloaded-p (sym buf)
-    "Return non-nil if function SYM is autoloaded."
-    (-when-let (file-name (buffer-file-name buf))
-      (setq file-name (s-chop-suffix ".gz" file-name))
-      (help-fns--autoloaded-p sym)))
-
-  (defun helpful--skip-advice (docstring)
-    "Remove mentions of advice from DOCSTRING."
-    (let* ((lines (s-lines docstring))
-           (relevant-lines
-            (--take-while
-             (not (or (s-starts-with-p ":around advice:" it)
-                      (s-starts-with-p "This function has :around advice:" it)))
-             lines)))
-      (s-trim (s-join "\n" relevant-lines)))))
-
-;; Display file commentary section
-(global-set-key (kbd "C-h C-c") 'finder-commentary)
+         ("C-h C-l" . find-library)
+         ;; Display file commentary section
+         ("C-h C-c" . finder-commentary)))
 
 ;;;;; Elisp Demos
 ;; Provide examples of Elisp code
