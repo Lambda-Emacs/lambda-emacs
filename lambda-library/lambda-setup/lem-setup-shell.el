b9a41c93d2f428c8fe81a56ffad5aa9351776fb2
Colin McLear <mclear@fastmail.com>
2022-10-03 21:18:58 -0400

    Add run in vterm functions

--- lambda-library/lambda-setup/lem-setup-shell.el
+++ lambda-library/lambda-setup/lem-setup-shell.el
@@ -129,10 +129,49 @@
         (vterm)
         (set-frame-parameter frame 'name "terminal")))))
 
+;;;; Run Command in Vterm
+;; See https://www.reddit.com/r/emacs/comments/ft84xy/run_shell_command_in_new_vterm/
+;; https://github.com/akermu/emacs-libvterm/pull/145
+
+(defun lem-run-in-vterm-kill (process event)
+  "A process sentinel. Kills PROCESS's buffer if it is live."
+  (let ((b (process-buffer process)))
+    (and (buffer-live-p b)
+         (kill-buffer b))))
+
+(defun lem-run-in-vterm (command)
+  "Execute string COMMAND in a new vterm.
+
+Interactively, prompt for COMMAND with the current buffer's file
+name supplied. When called from Dired, supply the name of the
+file at point.
+
+Like `async-shell-command`, but run in a vterm for full terminal features.
+
+The new vterm buffer is named in the form `*foo bar.baz*`, the
+command and its arguments in earmuffs.
+
+When the command terminates, the shell remains open, but when the
+shell exits, the buffer is killed."
+  (interactive
+   (list
+    (let* ((f (cond (buffer-file-name)
+                    ((eq major-mode 'dired-mode)
+                     (dired-get-filename nil t))))
+           (filename (concat " " (shell-quote-argument (and f (file-relative-name f))))))
+      (read-shell-command "Terminal command: "
+                          (cons filename 0)
+                          (cons 'shell-command-history 1)
+                          (list filename)))))
+  (with-current-buffer (vterm (concat "*" command "*"))
+    (set-process-sentinel vterm--process #'run-in-vterm-kill)
+    (vterm-send-string command)
+    (vterm-send-return)))
+
 ;;;; Multi-Vterm
 (use-package multi-vterm
   :commands (multi-vterm
-             multi-vterm-projectile
+             multi-vterm-project
              multi-vterm-dedicated-toggle))
 
 ;;; Virtualenvwrapper
