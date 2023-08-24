b47ad4f0d535739277a3a67682682609615a8003
Colin McLear <mclear@fastmail.com>
2023-08-24 09:18:20 -0500

    Fix tree sitter

--- lambda-library/lambda-setup/lem-setup-programming.el
+++ lambda-library/lambda-setup/lem-setup-programming.el
@@ -90,16 +90,33 @@
           org-mode markdown-mode
           eval-expression-minibuffer-setup) . puni-mode))
 
-;;;;; Tree Sitter
+;;;;;; Tree Sitter
+;; See https://www.masteringemacs.org/article/how-to-get-started-tree-sitter
 ;; Set load-path
 (setq treesit-extra-load-path `(,(concat lem-var-dir "tree-sitter/")))
-;; Automatically install & pick between tree-sitter and default major modes in Emacs 29+
-(use-package treesit-auto
-  :unless (version< emacs-version "29.0")
-  :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (global-treesit-auto-mode))
+;; List languages for install
+(setq treesit-language-source-alist
+      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+        (cmake "https://github.com/uyha/tree-sitter-cmake")
+        (css "https://github.com/tree-sitter/tree-sitter-css")
+        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+        (go "https://github.com/tree-sitter/tree-sitter-go")
+        (html "https://github.com/tree-sitter/tree-sitter-html")
+        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+        (json "https://github.com/tree-sitter/tree-sitter-json")
+        (make "https://github.com/alemuller/tree-sitter-make")
+        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+        (python "https://github.com/tree-sitter/tree-sitter-python")
+        (toml "https://github.com/tree-sitter/tree-sitter-toml")
+        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+;; Install all languages
+(defun lem-install-treesit-lang-grammar ()
+  "Install tree-sitter language grammars in alist."
+  (interactive)
+  (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist)))
 
 ;;;; Multiple Cursors
 (use-package iedit
