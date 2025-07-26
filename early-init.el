ff34deb909cd877e5072265e06eb13bcc5cdff3e
Colin McLear <mclear@fastmail.com>
2025-07-26 09:27:23 -0400

    feat: Add claudemacs package to package-vc-selected-packages
    - Add claudemacs package for Claude AI integration
    - Reformat package list for better readability

--- early-init.el
+++ early-init.el
@@ -318,11 +318,15 @@ Any customized libraries not available via standard package repos like elpa or m
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-vc-selected-packages
-   '((org-modern-indent :url "https://github.com/jdtsmith/org-modern-indent")
-     (org-devonthink :url "https://github.com/lasvice/org-devonthink" :branch "master")
-     (pulsing-cursor :url "https://github.com/jasonjckn/pulsing-cursor" :branch "main")
-     (bibtex-capf :url "https://github.com/mclear-tools/bibtex-capf.git" :branch "main")
-     (zotxt-emacs :url "https://github.com/egh/zotxt-emacs.git" :branch "master"))))
+   '((claudemacs :url "https://github.com/cpoile/claudemacs" :branch "main")
+     (zotxt-emacs :url "https://github.com/egh/zotxt-emacs.git" :branch "master")
+     (bibtex-capf :url "https://github.com/mclear-tools/bibtex-capf.git" :branch
+		  "main")
+     (pulsing-cursor :url "https://github.com/jasonjckn/pulsing-cursor" :branch
+		     "main")
+     (org-devonthink :url "https://github.com/lasvice/org-devonthink" :branch
+		     "master")
+     (org-modern-indent :url "https://github.com/jdtsmith/org-modern-indent"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
